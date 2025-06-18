9da9ec0ffd984d368cd81e54abb7d3791acfaa1c
imKrisK <imkrisk@icloud.com>
2025-06-17 18:31:30 -0700

    Initial commit

---
+++ resume-web/generate-pdf.js
@@ -0,0 +1,14 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto('file://' + __dirname + '/index.html', { waitUntil: 'networkidle' });
+  await page.pdf({
+    path: 'Kristoffer_Kelly_Resume.pdf',
+    format: 'A4',
+    printBackground: true,
+    margin: { top: '20mm', bottom: '20mm', left: '15mm', right: '15mm' }
+  });
+  await browser.close();
+})();
